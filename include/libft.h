97098375a3f8e11c58b16b3713d4632f2ccc486d
Greg PHILIPS <gphilips@e1r8p11.42.fr>
2016-11-05 21:30:26 +0100

    ajout des prototypes

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: gphilips <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 14:57:52 by gphilips          #+#    #+#             */
-/*   Updated: 2016/11/05 14:58:02 by gphilips         ###   ########.fr       */
+/*   Updated: 2016/11/05 21:25:25 by gphilips         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define LIBFT_H
 
 # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(void *b, int c, size_t len);
 void 	ft_bzero(void *s, size_t n);
@@ -21,4 +23,28 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
+
+
+
+
+
+
+
+
+
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *nptr);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 #endif
