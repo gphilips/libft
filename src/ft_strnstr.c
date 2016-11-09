9729b5423ae49fab737455deb2f50dfd521e7295
Greg Philips <gregphilips@macbook-pro-de-greg.home>
2016-11-09 23:47:35 +0100

    ajout strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -3,12 +3,24 @@
 char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	size_t	k;
+	size_t	tmp;
 
 	i = 0;
+	k = 0;
+	if (s2[i] == '\0' || !s2)
+		return ((char*)s1);
 	while (s1[i] && i < n)
 	{
-		if (ft_memcmp(&s1[i], s2, ft_strlen(s2)) == 0)
-			return ((char*)(&s1[i]));
+		if (s1[i] == s2[k])
+		{
+			tmp = i;
+			while (s2[k] && s1[i + k] == s2[k])
+				k++;
+			i = tmp;
+		}
+		if (s2[k] == '\0')
+			return ((char*)&s1[i]);
 		i++;
 	}
 	return (NULL);
