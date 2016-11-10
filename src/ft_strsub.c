490de5afbfaeeb099a494d2431e2d16433208e95
Greg Philips <gregphilips@macbook-pro-de-greg.home>
2016-11-10 02:55:11 +0100

    ajout strsub

---
+++ src/ft_strsub.c
@@ -0,0 +1,21 @@
+#include "../include/libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	str = ft_strnew(len);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	return (str);
+}
