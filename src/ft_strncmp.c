4821996325fa09f19a79216db33f2d47d51a664c
Greg Philips <gregphilips@macbook-pro-de-greg.home>
2016-11-12 23:12:28 +0100

    correction du ft_strncmp

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -18,10 +18,10 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char *str2;
 	size_t	i;
 
-	str1 = (unsigned char*) s1;
-	str2 = (unsigned char*) s2;
+	str1 = (unsigned char*)s1;
+	str2 = (unsigned char*)s2;
 	i = 0;
-	while (str1[i] && str1[i] == str2[i] && i < n)
+	while (str1[i] && (str1[i] == str2[i]) && i < n)
 		i++;
 	return (str1[i] - str2[i]);
 }
