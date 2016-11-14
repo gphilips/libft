5cae355bf025a26f8b4e31fc7f3b0fb0a6d4f0e9
Greg PHILIPS <gphilips@e1r12p22.42.fr>
2016-11-14 20:28:20 +0100

    modification du proto atoi

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: gphilips <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 14:57:52 by gphilips          #+#    #+#             */
-/*   Updated: 2016/11/08 21:13:02 by gphilips         ###   ########.fr       */
+/*   Updated: 2016/11/14 15:16:57 by gphilips         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*ft_strstr(const char *s1, const char *s2);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_atoi(const char *nptr);
+int		ft_atoi(const char *s);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
