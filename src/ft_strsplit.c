c6e4fb95b3cecb09b370fa745bfdf801053a3eb8
Greg Philips <gregphilips@macbook-pro-de-greg.home>
2016-11-15 13:12:12 +0100

    strsplit OK (manque norme)

--- src/ft_strsplit.c
+++ src/ft_strsplit.c
@@ -12,83 +12,68 @@
 
 #include "../include/libft.h"
 
-static size_t	ft_wordlen(char const *s, char c)
+static int		ft_count_word(const char *s, char c)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] && s[i] != c)
-		i++;
-	return (i);
-}
-
-static size_t		ft_count_word(char const *s, char c)
-{
-	size_t	i;
-	size_t	count;
-	size_t	is_word;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-	is_word = 0;
+	if (s[0] != '\0')
+		count++;
 	while (s[i])
 	{
-		if (s[i] != c && is_word == 0)
-		{
+		if (s[i] == c && s[i + 1] != c)
 			count++;
-			is_word = 1;
-		}
-		else if (s[i] == c && is_word == 1)
-			is_word = 0;
 		i++;
 	}
 	return (count);
 }
 
-static unsigned int		ft_start_word(char const *s, char c)
+static	char	*ft_get_word(const char *s, char c, int *i)
 {
-	size_t	i;
+	char	*tab;
+	int		j;
 
-	i = 0;
-	while (s[i])
+	tab = (char *)malloc(sizeof(tab) * (ft_strlen(s)));
+	if (!tab)
+		return (NULL);
+	j = 0;
+	while (s[*i] != c && s[*i])
 	{
-		if (s[i] != c)
-			return (i);
-		i++;
+			tab[j] = s[*i];
+			j++;
+			*i += 1;
 	}
-	return (0);
-}
-
-static char				*ft_get_word(char const *s, char c)
-{
-	size_t			wordlen;
-	unsigned int	start;
-	char			*tab;
-
-	wordlen = ft_wordlen(s, c);
-	start = ft_start_word(s, c);
-	tab = ft_strsub(s, start, wordlen);
+	tab[j] = '\0';
+	while (s[*i] == c && s[*i])
+			*i += 1;
 	return (tab);
 }
 
-char					**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(const char *s, char c)
 {
-	size_t	i;
-	size_t	nb_word;
+	int		nb_word;
 	char	**tab;
+	int		i;
+	int		j;
 
 	if (!s || !c)
 		return (NULL);
 	nb_word = ft_count_word(s, c);
-	tab = (char**)malloc(sizeof(char*) * nb_word + 1);
+	tab = (char**)malloc(sizeof(tab) * (nb_word + 1));
 	if (!tab)
 		return (NULL);
 	i = 0;
-	while (i <= nb_word)
-	{
-		tab[i] = ft_get_word(s, c);
+	j = 0;
+	while (s[i] == c && s[i])
 		i++;
+	while (j < nb_word && s[i])
+	{
+		tab[j] = ft_get_word(s, c, &i);
+		j++;
 	}
-	tab[i] = NULL;
+	tab[j] = NULL;
 	return (tab);
 }
+
